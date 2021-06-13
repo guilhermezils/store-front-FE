695c32e13dfce4f02fd2d25b30178cb12609da2b
guilhermezils <guilhermezils@icloud.com>
2021-06-13 17:25:45 -0500

    reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import ScoreColletion from "./containers/ScoreCollection";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ScoreColletion />
+        blah
       </div>
     );
   }
